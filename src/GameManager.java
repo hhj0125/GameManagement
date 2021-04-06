a7b866215eff543162f455dba88f2e01f9719b67
hhj0125 <gjgywns0125@naver.com>
2021-04-06 16:43:53 +0900

    GameManager

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,33 +1,31 @@
-
-
 import java.util.Scanner;
 
 public class GameManager {
+   public static void main(String[] args) {
+	   Scanner input = new Scanner(System.in);
+	   UserManager userManager = new UserManager(input);
+	   
+		int num = -1;
 
-	public static void main(String[] args) {
-		int num = 0;
-		Scanner input = new Scanner(System.in);
-
-        while (num != 6) {		
+        while (num != 5) {		
 		System.out.println("1. Add User");
 		System.out.println("2. Delete User");
 		System.out.println("3. Edit User");
 		System.out.println("4. View User");
-		System.out.println("5. Show a Menu");
-		System.out.println("6. Exit");
+		System.out.println("5. Exit");
 		System.out.println("Select one number betwwn 1-6:");
 		num = input.nextInt();
 		if (num == 1) {
-			addUser();
+			userManager.addUser();
 		}
 		else if (num == 2) {
-			deleteUser();
+			userManager.deleteUser();
 		}
 		else if (num == 3) {
-			editUser();
+			userManager.editUser();
 		}
 		else if (num == 4) {
-			viewUser();
+			userManager.viewUser();
 		}
 		else {
 			continue;
@@ -35,37 +33,4 @@ public class GameManager {
 		  }
 		}
 	}
-	public static void addUser() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("User ID:");
-		int UserId = input.nextInt();
-		System.out.print("User name:");
-		String Username = input.next();
-		System.out.print("User class");
-		String Userclass = input.next();
-		System.out.print("User LV:");
-        int UserLV = input.nextInt();
-        System.out.print("User phonenumber");
-        int phone = input.nextInt();
-        System.out.print("When User Start?");
-        int day = input.nextInt();
-		
-	}
-	public static void deleteUser() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("User ID:");
-		int UserId = input.nextInt();
-	
-	}
-	public static void editUser() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("User ID:");
-		int UserId = input.nextInt();
-	}
-	public static void viewUser() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("User ID:");
-		int UserId = input.nextInt();
-	}
-	
 }
